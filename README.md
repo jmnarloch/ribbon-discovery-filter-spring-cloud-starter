66710c995c4ae1ae953d9a37d94bc5e438001c1c
Jakub Narloch <jmnarloch@gmail.com>
2015-11-16 00:50:49 +0100

    Initial source code commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Spring Cloud Ribbon Discovery Server filter
+
+> A Spring Cloud Ribbon extension for filtering the
+
+[![Build Status](https://travis-ci.org/jmnarloch/ribbon-discovery-filter-spring-cloud-starter.svg?branch=master)](https://travis-ci.org/jmnarloch/ribbon-discovery-filter-spring-cloud-starter)
+[![Coverage Status](https://coveralls.io/repos/jmnarloch/ribbon-discovery-filter-spring-cloud-starter/badge.svg?branch=master&service=github)](https://coveralls.io/github/jmnarloch/ribbon-discovery-filter-spring-cloud-starter?branch=master)
+
+## Features
+
+## Setup
+
+Add the Spring Cloud starter to your project:
+
+```xml
+<dependency>
+  <groupId>io.jmnarloch</groupId>
+  <artifactId>ribbon-discovery-filter-spring-cloud-starter</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+
+## Properties
+
+
+## License
+
+Apache 2.0
---
+++ src/main/java/io/jmnarloch/spring/cloud/ribbon/MetadataServerListMatcher.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2015 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jmnarloch.spring.cloud.ribbon;
+
+import com.netflix.niws.loadbalancer.DiscoveryEnabledServer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+public class MetadataServerListMatcher implements DiscoveryEnabledServerListMatcher {
+
+    @Override
+    public List<DiscoveryEnabledServer> matchServers(List<DiscoveryEnabledServer> servers) {
+
+        final ServerMatcher serverMatcher = new ServerMatcher(RibbonDiscoveryFilterContextHolder.getCurrentContext(), servers);
+        return serverMatcher.match();
+    }
+
+    private static class ServerMatcher {
+
+        private final RibbonDiscoveryFilterContext filterContext;
+
+        private final List<DiscoveryEnabledServer> servers;
+
+        public ServerMatcher(RibbonDiscoveryFilterContext filterContext, List<DiscoveryEnabledServer> servers) {
+            this.filterContext = filterContext;
+            this.servers = servers;
+        }
+
+        public List<DiscoveryEnabledServer> match() {
+            List<DiscoveryEnabledServer> matching = new ArrayList<>();
+            for(DiscoveryEnabledServer server : servers) {
+                final Map<String, String> metadata = server.getInstanceInfo().getMetadata();
+                for(Map.Entry<String, String> entry : filterContext.getAttributes().entrySet()) {
+                    String value = metadata.get(entry.getKey());
+                    if(value != null && value.equals(entry.getValue())) {
+                        matching.add(server);
+                    }
+                }
+            }
+            return matching;
+        }
+    }
+}
